41e91c73d794ebbb78b726c6cf84a51cd1eccc81
mmdatong <mmdatong@aliyun.com>
2023-11-23 22:39:58 +0800

    modify create_arithmetic_dataset.sh, fix wrong name: arithmetric to arithmetic

--- arithmetic_data/create_arithmetic_dataset.sh
+++ arithmetic_data/create_arithmetic_dataset.sh
@@ -1,13 +1,13 @@
 
 python create_arithmetic_dataset.py
-head -n 9501 data.csv > arithmetric_train.csv
+head -n 9501 data.csv > arithmetic_train.csv
 
 
-rm arithmetric_validation.csv
-head -n 1 data.csv >> arithmetric_validation.csv
-tail -n 500 data.csv >> arithmetric_validation.csv
+rm arithmetic_validation.csv
+head -n 1 data.csv >> arithmetic_validation.csv
+tail -n 500 data.csv >> arithmetic_validation.csv
 
-rm arithmetric_test.csv
-cp arithmetric_validation.csv arithmetric_test.csv
+rm arithmetic_test.csv
+cp arithmetic_validation.csv arithmetic_test.csv
 
 
